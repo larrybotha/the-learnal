f0bb0397bfaf763995844c83b96d9a7094f59875
Larry Botha <larry@fixate.it>
2016-07-08 01:28:13 +0200

    install and configure pug for layout and templtes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var Metalsmith  = require('metalsmith');
-var markdown    = require('metalsmith-markdown');
-var layouts     = require('metalsmith-layouts');
-var permalinks  = require('metalsmith-permalinks');
+const Metalsmith  = require('metalsmith');
+const markdown    = require('metalsmith-markdown');
+const layouts     = require('metalsmith-layouts');
+const permalinks  = require('metalsmith-permalinks');
 
 Metalsmith(__dirname)
-  .source('./src')
-  .destination('./build')
-  .clean(false)
+  .clean(true)
   .use(markdown())
-  .use(permalinks())
   .use(layouts({
-    engine: 'handlebars'
+    engine: 'pug',
+    directory: 'layouts',
+    partials: 'partials'
   }))
+  .destination('./build')
   .build(function(err, files) {
     if (err) { throw err; }
   });
